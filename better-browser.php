5fbfe8de622aa992bfea2f887620dd397ea8fe26
Pepijn Nichting <pepijn.nichting@gmail.com>
2020-01-08 15:26:22 +0000

    better-browser.php edited online with Bitbucket

--- better-browser.php
+++ better-browser.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Better Browser
 Description: Add front-end notification bar for visitors using IE.
-Version: 0.3.2
+Version: 0.4.1
 Author: Pepijn Nichting
 Text Domain: betterbrowser
 Domain Path: /languages
